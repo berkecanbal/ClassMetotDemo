75a6354cfbcc99084ed2aa88aa5095a767b06765
Berke Can Bal <berkebaltr@hotmail.com>
2021-01-18 18:56:36 +0300

    1

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi" + " : " + musteri.Ad + "  " + musteri.Soyad);
+
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi" + " : " + musteri.Ad + "  " + musteri.Soyad);
+
+        }
+
+        public void Bilgi(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Bilgileri" + " : " + musteri.Id + "  " + musteri.Ad + "  " + musteri.Soyad + "  " + musteri.dogumyili + "  " + musteri.Sehir);
+
+        }
+            
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,39 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 0001;
+            musteri1.Ad = "Muhittin";
+            musteri1.Soyad = "Topalak";
+            musteri1.dogumyili = 1983;
+            musteri1.Sehir = "İstanbul";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 0002;
+            musteri2.Ad = "Zafer";
+            musteri2.Soyad = "Algöz";
+            musteri2.dogumyili = 1961;
+            musteri2.Sehir = "Kars";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 0003;
+            musteri3.Ad = "Berke Can";
+            musteri3.Soyad = "Bal";
+            musteri3.dogumyili = 2001;
+            musteri3.Sehir = "İstanbul";
+
+            Musteri musteri4 = new Musteri();
+            musteri4.Id = 0004;
+            musteri4.Ad = "Gazanfer";
+            musteri4.Soyad = "Nevizade";
+            musteri4.dogumyili = 1951;
+            musteri4.Sehir = "İstanbul";
+
+            MusteriManager musterimanager = new MusteriManager();
+            musterimanager.Ekle(musteri1);
+            musterimanager.Sil(musteri2);
+            musterimanager.Bilgi(musteri4);
+
         }
     }
 }
